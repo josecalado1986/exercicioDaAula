44765cf40b67bd57041381d0959feced5ff5e357
josecalado1986 <138127167+josecalado1986@users.noreply.github.com>
2023-07-20 19:13:30 +0000

    msg

---
+++ condicional.js
@@ -0,0 +1,35 @@
+let numero =0;
+if (numero <0){
+    console.log("numero é negativo");
+}else{ 
+    console.log("numero e positivo");
+}
+
+
+
+let numero1 =1;
+if(numero1%2==0){
+    console.log("e um numero par");
+}else{
+    console.log("numero impar");
+}
+let numero2 =18
+if(numero2>10){
+    console.log("numero é maior que dez")
+} else{
+    console.log("numero e menor que 10");
+
+}
+ let letras="a,b,c,";
+ if(letras==null){
+    console.log("variavel vazia")
+ } else{
+    console.log("a variavel nao e vazia");
+ }
+
+ let bol = true
+ if( typeof bol === "boolean"){
+    console.log("e boleano");
+ }else{
+    console.log("nao é boleano");
+ }
