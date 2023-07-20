73100cb85619135a77760dadb617bb5683137121
josecalado1986 <138127167+josecalado1986@users.noreply.github.com>
2023-07-20 18:19:42 +0000

    mesagem

---
+++ exercicio.js
@@ -0,0 +1,36 @@
+let minhaVar
+
+minhaVar = 10;
+console.log(minhaVar);
+let soma = 15 + 8 ;
+console.log (soma);
+soma += 1;
+console.log(soma);
+soma *= 3;
+console.log(soma);
+
+
+let fazSol =true;
+console.log(fazSol);
+let comida=["arroz","feijão","ovo"];
+console.log(comida[1]);
+
+if(soma === minhaVar){
+    console.log("é iqual");
+}else{
+    console.log("é diferente");
+}
+
+if(minhaVar <= soma){
+    console.log(" é menor");
+
+}else{
+    console.log("nao e menor");
+}
+ 
+function  divisão (n1,n2){
+    return n1/n2
+}
+console.log(divisão(10,2));
+    
+
