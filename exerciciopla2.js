5c060f8a0d246351e232df2f9d2d537dad848563
josecalado1986 <138127167+josecalado1986@users.noreply.github.com>
2023-07-22 23:31:50 +0000

    msg

--- exerciciopla2.js
+++ exerciciopla2.js
@@ -1,17 +1,17 @@
-const input = requise ("readline-sync");
+const input=("readlile-sysnc");
 
 let nome = input.question(" digite seu nome :");
 let nota1 = parseFloat(input.question("digite a primeira nota : "));
 let nota2 =parsefloat(input.question("digite a segunda nota :"));
-let quantidadeFalta = input.question("digite a quantidade de falta");
+let faltas = input.question("digite a quantidade de falta");
 let media = (nota1 + nota2)/2;
 
-//if( media < 7){
-  //  console.log(nome ," você foi reprovado", );
+if( media < 7){
+    console.log(" você foi reprovado" );
 
-//}else if( quantidadeFalta > 3){
-    console.log(nome ,"você foi reprovado" );
-//}else(media >= 7){
-  //  console.log ( nome ,"você foi aprovado")
-//}
+}else if( faltas > 3){
+    console.log("você foi reprovado" );
+}else(media >= 7){
+  console.log ( "você foi aprovado")
+}
 console.log(media);
