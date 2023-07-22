e7ada20836feb3d4f1abd85d40504f23b6479bee
josecalado1986 <138127167+josecalado1986@users.noreply.github.com>
2023-07-22 21:59:56 +0000

    msg

---
+++ exerciciopla.js
@@ -0,0 +1,8 @@
+let nome = "nome";
+console.log("qual o tipo da variavel" , typeof nome, nome);
+let idade = 5;
+console.log("qual o tipo da variavel" , typeof idade, idade);
+let altura = 1.68;
+console.log("qual o tipo da variavel" , typeof altura, altura);
+let maior_de_idade= true;
+console.log("qual o tipo da variavel" , typeof maior_de_idade, maior_de_idade );
