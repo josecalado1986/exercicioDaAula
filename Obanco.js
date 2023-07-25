4b889a3594a0a7096f32a49828b6f07729c94171
josecalado1986 <138127167+josecalado1986@users.noreply.github.com>
2023-07-25 19:54:33 +0000

    msg

---
+++ Obanco.js
@@ -0,0 +1,5 @@
+let carta1 = Math.round(math.random()*100)
+let carta2 = Math.round(math.random()*100)
+let carta3 = Math.round(math.random()*100)
+let carta4 = Math.round(math.random()*100)
+let = carta1 + carta2 + carta3 + carta4;
