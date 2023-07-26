7495124a003a64ebc3efce9373a00cfb926b8846
josecalado1986 <138127167+josecalado1986@users.noreply.github.com>
2023-07-26 01:07:11 +0000

    msg

--- Obanco.js
+++ Obanco.js
@@ -1,16 +1,24 @@
 const input= require("readline-sync");
-let carta1= input.questionInt("digite sua carta");
-let carta2= input.questionInt(" digite sua carta ");
+let carta1= input.questionInt("digite sua carta : ");
+let carta2= input.questionInt(" digite sua carta : ");
 let soma = carta1 + carta2;
-if( soma >21){
-    console.log(" jogador perdeu !!")
-}else{ 
-    console.log( "voce ganhou !!");
-} 
-console.log(" seus pontos saõ :", soma);
 let c1= (Math.round(Math.random()*10));
 let c2 = (Math.round(Math.random()*10));
 let c3 = (Math.round(Math.random()*10));
 let c4 = (Math.round(Math.random()*10));
 let somaBanco = c1 + c2 + c3 + c4;
-console.log("a soma das cartaz do banco são : ",somaBanco);
+while(true){
+    if(somaBanco >21 || soma <= 21 && soma > somaBanco){
+        console.log("jogador venceu!!");
+        break;
+    }
+    }
+        
+    console.log("você perdeu !!!");
+    console.log("tente novamente, seus pontos :" , soma);
+    console.log(" pontos do banco :",somaBanco)
+
+
+
+
+
