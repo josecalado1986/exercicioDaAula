021bd7fba27255c5e56d1f000e6fc13256374278
josecalado1986 <138127167+josecalado1986@users.noreply.github.com>
2023-07-30 00:46:02 +0000

    msg

--- calculadora1.js
+++ calculadora1.js
@@ -1,27 +1,27 @@
-const input =require ("readline-sync");
+const input = require("readline-sync");
 
-function calculadora (){
-  let n1 =input.questionInt ("digite o primeiro numero :  " );
-  let n2 = input.questionInt ("digite o segundo numero:  " );
-  let operacao =input.questionInt("digite a operação desejada: 1 - soma , 2 - subtração , 3 - multiplicacão , 4 - divisão , : ");
-  while (operacao){
-    if (operacao === 1){
-      console.log(" o valor da soma e :" + (n1 + n2) );
-    }else if(operacao ===2){
-      console.log(" o valor da subtracao e :"+ (n1 - n2))
-       
-    }else if(operacao ===3){
-      console.log("o valor da multiplicacão e :"+ (n1 * n2));
-    }else if (operacao === 4){
-      console.log("o valor da subtracao e "+ (n1/n2));
-    }else{
+function calculadora() {
+  let n1 = input.questionInt("digite o primeiro numero :  ");
+  let n2 = input.questionInt("digite o segundo numero:  ");
+  let operacao = input.questionInt("digite a operação desejada: 1 - soma , 2 - subtração , 3 - multiplicacão , 4 - divisão , : ");
+  while (operacao) {
+    if (operacao === 1) {
+      console.log(" o valor da soma e :" + (n1 + n2));
+    } else if (operacao === 2) {
+      console.log(" o valor da subtracao e :" + (n1 - n2))
+
+    } else if (operacao === 3) {
+      console.log("o valor da multiplicacão e :" + (n1 * n2));
+    } else if (operacao === 4) {
+      console.log("o valor da subtracao e " + (n1 / n2));
+    } else {
       console.log(" operacao invalida");
     }
-    let sair =input.question("sair ?");
-    if(sair == sim){
-      break
-    }else if(sair == nao) {
-  
+    let sair = input.question("sair ?");
+    if (sair == "sim") {
+      return;
+    } else if (sair == "nao") {
+
       calculadora();
     }
   }
