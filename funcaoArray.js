5c0987f54b4d3117cb537e1c65c0656f36ad9939
josecalado1986 <138127167+josecalado1986@users.noreply.github.com>
2023-08-03 13:01:39 +0000

    msg

---
+++ funcaoArray.js
@@ -0,0 +1,13 @@
+//function  divisão (n1,n2){
+  //  return n1/n2
+//}
+//console.log(divisão(10,2));
+
+const num = [1,2,3,4,4,5,];
+
+
+function parametro (x){
+    return(num)
+}
+
+console.log(Array.isArray(num));
