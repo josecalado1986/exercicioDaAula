6fe719d5e021ff504ad63520938af155c480b22c
josecalado1986 <138127167+josecalado1986@users.noreply.github.com>
2023-08-08 19:41:46 +0000

    msg

---
+++ funcao.js
@@ -0,0 +1,56 @@
+
+const input = require("readline-sync")
+
+//function  saudacao(){
+ //   console.log("Olá mundo!")
+//}
+//saudacao();
+
+//dobro do numero.
+//let num = input.questionInt(" digite um numero :")
+//function dobro(num){
+  //  console.log(num * num)
+
+//}
+//dobro(num);
+
+
+//recebendo intervalo de entre inicio e fim.
+//criei as duas variaveis e depois criei uma funcao.
+//let inicio= input.questionInt(" digite um numero inicial :")
+//let fim = input.questionInt(" digite um numero final :")
+//function mostrarNumero(inicio , fim){
+  //  for(i=inicio ; i< fim ;i++){
+    //    console.log(i)
+    //}
+    
+
+//}
+
+//mostrarNumero(inicio,fim)
+
+
+// verificar se o numeroi e par.
+//let  num1 = input.question("digite um numero");
+
+//function verificarpar(num1){
+    //if(num1%2==0){
+      //  console.log(`numero é par ${num1}`)
+   // }else{
+      //  console.log(`nao é um numero par ${num1}`);
+    //}
+//}
+//verificarpar(num1);
+
+let lista= ["carne" , "frutas", "ovos", "uva" ]
+
+function imprimirLista(lista){
+    for(let i=0 ; i < lista.length ;i++){
+    console.log(lista)}
+}
+imprimirLista(lista[0]);
+imprimirLista(lista[1]);
+imprimirLista(lista[2]);
+imprimirLista(lista[3]);
+
+
