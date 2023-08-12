c085ac8ed7b630c9f0c25210ea391763492d6a20
josecalado1986 <138127167+josecalado1986@users.noreply.github.com>
2023-08-12 00:29:23 +0000

    msg

---
+++ matrizes.js
@@ -0,0 +1,10 @@
+let matriz = [
+    ["cachorro", "mamífero", 5],
+    ["gato", "mamífero", 3],
+    ["papagaio", "ave", 2]
+  ];
+  for(let i = 0 ;  i < matriz.length; i++){
+    for(let j = 0 ; j < matriz[i].length ; j++){
+        console.log(matriz[i][j]);
+    }
+  }
